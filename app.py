bceba70c7807c47d793110bf64881b2a0379d496
Rininmmmment <101512834+Rininmmmment@users.noreply.github.com>
2022-09-26 06:57:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,25 +9,27 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing.image import array_to_img, img_to_array, load_img, ImageDataGenerator
 import pandas as pd
 import math
-train_dir = 'target_datasets/train' #trainフォルダをtrain_dirとして設定
-val_dir = 'target_datasets/val' #valフォルダをval_dirとして設定
-backup_dir = './model' #modelフォルダをbackup_dirとして設定
+train_dir = 'target_datasets/train' 
+val_dir = 'target_datasets/val' 
+backup_dir = './model' 
 BATCH_SIZE = 32
 IMAGE_SIZE = 64
 val_data_gen = ImageDataGenerator(rescale=1./255)
-model = load_model('puyo_model.h5')
+model = load_model('my_model.h5')
 # korekamoを名前に変換
 def korekana(num):
   if num == 0:
-    korename = "赤"
+    korename = "ツチガエル"
   elif num == 1:
-    korename = "緑"
+    korename = "トノサマガエル"
   elif num == 2:
-    korename = "黄"
+    korename = "アカメアマガエル"
   elif num == 3:
-    korename = "青"
+    korename = "モリアオガエル"
   elif num == 4:
-    korename = "紫"
+    korename = "イチゴヤドクガエル"
+  elif num == 5:
+    korename = "アマガエル"
   return korename
 st.set_page_config(
      page_title="ImgTest",
@@ -41,7 +43,7 @@ st.set_page_config(
 
 
 
-st.title("ぷよぷよを見分けよう(デモ版)")
+st.title("カエルを見分けよう")
 
 reset = st.button("写真をリセットする")
 if reset and os.path.isfile("target_datasets/val2/1/imported_file.jpg"):
@@ -49,9 +51,8 @@ if reset and os.path.isfile("target_datasets/val2/1/imported_file.jpg"):
 else:
     pass
 
-# ファイルアップロード
 st.write("")
-uploaded_file=st.file_uploader("ファイルアップロード (7KBまでなら正常にアップロードできます。)", accept_multiple_files=False)
+uploaded_file=st.file_uploader("ファイルアップロード", accept_multiple_files=False)
 if uploaded_file:
  image=Image.open(uploaded_file)
  img_array3 = np.array(image)
@@ -75,7 +76,6 @@ else:
     pass
 
 if os.path.isfile("target_datasets/val2/1/imported_file.jpg"):
-    #推論結果を格納
     val_dir2 = "target_datasets/val2"
     predictdata = val_data_gen.flow_from_directory(
         val_dir2, target_size=(IMAGE_SIZE, IMAGE_SIZE),
@@ -87,13 +87,11 @@ if os.path.isfile("target_datasets/val2/1/imported_file.jpg"):
     except:
         pass
     
-    # 結果表示
-    # st.image("target_datasets/val2/1/imported_file.jpg", width = 200)
-    st.write("### この写真は"+str(math.floor(suiron[0][korekamo]*100))+"%の確率で ---"+(str(korekana(korekamo))+"ぷよ--- だよ。"))
+    st.write("### この写真は"+str(math.floor(suiron[0][korekamo]*100))+"%の確率で ---"+(str(korekana(korekamo))+"--- だよ。"))
     st.write("")
-    st.write("↓その他の推論結果")
-    arr = [suiron[0][0], suiron[0][1], suiron[0][2], suiron[0][3], suiron[0][4]]
-    label = ["Red"," Green","Yellow","Blue","Purple"]
+    st.write("↓　その他の推論結果")
+    arr = [suiron[0][0], suiron[0][1], suiron[0][2], suiron[0][3], suiron[0][4], suiron[0][5]]
+    label = ["Tsuchi"," Tono","Akame","Mori","Ichigo", "Ama"]
     fig, ax = plt.subplots()
     ax.bar(label, arr)
     st.pyplot(fig)
